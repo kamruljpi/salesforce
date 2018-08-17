5aee6e5d5cfe5efa58df5c55f423f92b2a399426
Shohidur Rahman <sohidurr49@gmail.com>
2018-08-17 11:52:14 +0600

    change

---
+++ idlc-sales-force/app/Model/IfaManagement/RejectionRemarksModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model\IfaManagement;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RejectionRemarksModel extends Model
+{
+    protected $table = "tbl_rejection_remarks";
+}
